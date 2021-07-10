3751a021e4d7f2748da373f276c3ca4f5f0acd80
navjeetgill <navjeetgill240297@gmail.com>
2021-07-10 22:12:28 +0530

    as

--- yah/create.tf
+++ yah/create.tf
@@ -4,6 +4,6 @@ provider "aws" {
   region     = "us-east-2"
 }
 
-resource "aws_instance" "MyFirstInstnace" {
+resource "aws_instance" "MyFiirstInstnace" {
   ami           = "ami-0d563aeddd4be7fff"
   instance_type = "t2.micro"
