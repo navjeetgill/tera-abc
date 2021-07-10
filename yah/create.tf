8d3b438c76939af998b6405add6ecc31c3361379
navjeetgill <navjeetgill240297@gmail.com>
2021-07-10 22:04:06 +0530

    secnd cmmt

---
+++ yah/create.tf
@@ -0,0 +1,9 @@
+provider "aws" {
+  access_key = "AKIA5ZRJOLOFR5ZQX7GK"
+  secret_key = "SECRET_KEY_HERE"
+  region     = "us-east-2"
+}
+
+resource "aws_instance" "MyFirstInstnace" {
+  ami           = "ami-0d563aeddd4be7fff"
+  instance_type = "t2.micro"
